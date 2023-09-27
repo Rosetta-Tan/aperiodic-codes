6f52978cc3a1c5953649eed71c83b6dbd20cf66b
Rosetta-Tan <rosetta_tan@outlook.com>
2023-09-26 21:58:24 -0400

    HGP doesn't work for Haah's code

--- src/gen_haah.py
+++ src/gen_haah.py
@@ -1,11 +1,8 @@
 import numpy as np
-from ldpc import code_util
+from ldpc.code_util import get_code_parameters
 from bposd.hgp import hgp
+from bposd.css import *
 from numba import njit
-import networkx as nx
-from networkx.algorithms import bipartite
-import matplotlib as mpl
-from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
 
@@ -15,11 +12,16 @@ n = L**3
 sqrtL = int(np.sqrt(L))
 
 ####################################################################################################
-# Generate Haah's code. (ref: https://arxiv.org/abs/1101.1962)
+# Generate Haah's code. (ref: https://www.overleaf.com/project/64c9d73ea1c40344fe0756e7)
 ####################################################################################################
+'''Caveat:
+We cannot use two periodic boundary classical codes to obtain Haah's code.
+Instead, we use two OBC classical codes to do HGP,
+then take the PBC at the end.
+'''
 
 @njit
-def h1():
+def h1(sqrtL):
     '''The first 3 numbers are positions of checks,
     (i, j, k) represents cube centered at (i+1/2, j+1/2, k+1/2).
     The last 3 are positions of bits,
@@ -29,40 +31,71 @@ def h1():
     c(i+1/2, j+1/2, k+1/2) -> v(i, j+1, k+1)
     c(i+1/2, j+1/2, k+1/2) -> v(i+1, j, k+1)
     c(i+1/2, j+1/2, k+1/2) -> v(i+1, j+1, k)
+    Note:
+    Ghost bits are added to implement OBC.
     '''
-    h1 = np.zeros((sqrtL, sqrtL, sqrtL, sqrtL, sqrtL, sqrtL), dtype=np.uint8)
+    h1 = np.zeros((sqrtL, sqrtL, sqrtL, sqrtL+1, sqrtL+1, sqrtL+1), dtype=np.uint8)
     for i in range(sqrtL):
         for j in range(sqrtL):
             for k in range(sqrtL):
-                h1[i, j, k, (i+1)//sqrtL, (j+1)//sqrtL, (k+1)//sqrtL] = 1
-                h1[i, j, k, i, (j+1)//sqrtL, (k+1)//sqrtL] = 1
-                h1[i, j, k, (i+1)//sqrtL, j//L, (k+1)//sqrtL] = 1
-                h1[i, j, k, (i+1)//sqrtL, (j+1)//sqrtL, k//sqrtL] = 1
-    return h1.reshape((sqrtL**3, sqrtL**3))
+                # h1[i, j, k, (i+1)//sqrtL, (j+1)//sqrtL, (k+1)//sqrtL] = 1
+                # h1[i, j, k, i, (j+1)//sqrtL, (k+1)//sqrtL] = 1
+                # h1[i, j, k, (i+1)//sqrtL, j//L, (k+1)//sqrtL] = 1
+                # h1[i, j, k, (i+1)//sqrtL, (j+1)//sqrtL, k//sqrtL] = 1
+                h1[i,j,k, i+1,j+1,k+1] = 1
+                h1[i,j,k, i,j+1,k+1] = 1
+                h1[i,j,k, i+1,j,k+1] = 1
+                h1[i,j,k, i+1,j+1,k] = 1
+    return h1.reshape((sqrtL**3, (sqrtL+1)**3))
 
 @njit
-def h2():
+def h2(sqrtL):
     '''Connection:
     c(i+1/2, j+1/2, k+1/2) -> v(i, j, k)
     c(i+1/2, j+1/2, k+1/2) -> v(i+1, j+1, k)
     c(i+1/2, j+1/2, k+1/2) -> v(i+1, j, k+1)
     c(i+1/2, j+1/2, k+1/2) -> v(i, j+1, k+1)
     '''
-    h2 = np.zeros((sqrtL, sqrtL, sqrtL, sqrtL, sqrtL, sqrtL), dtype=np.uint8)
+    h2 = np.zeros((sqrtL, sqrtL, sqrtL, sqrtL+1, sqrtL+1, sqrtL+1), dtype=np.uint8)
     for i in range(sqrtL):
         for j in range(sqrtL):
             for k in range(sqrtL):
-                h2[i, j, k, i, j, k] = 1
-                h2[i, j, k, (i+1)//sqrtL, (j+1)//sqrtL, k] = 1
-                h2[i, j, k, (i+1)//sqrtL, j, (k+1)//sqrtL] = 1
-                h2[i, j, k, i, (j+1)//sqrtL, (k+1)//sqrtL] = 1
-    return h2.reshape((sqrtL**3, sqrtL**3))
-h1, h2 = h1(), h2()
-haah_code = hgp(h1, h2)
+                # h2[i, j, k, i, j, k] = 1
+                # h2[i, j, k, (i+1)//sqrtL, (j+1)//sqrtL, k] = 1
+                # h2[i, j, k, (i+1)//sqrtL, j, (k+1)//sqrtL] = 1
+                # h2[i, j, k, i, (j+1)//sqrtL, (k+1)//sqrtL] = 1
+                h2[i,j,k, i,j,k] = 1
+                h2[i,j,k, i+1,j+1,k] = 1
+                h2[i,j,k, i+1,j,k+1] = 1
+                h2[i,j,k, i,j+1,k+1] = 1
+    return h2.reshape((sqrtL**3, (sqrtL+1)**3))
+h1, h2 = h1(sqrtL), h2(sqrtL)
 
-####################################################################################################
-# Save data
-####################################################################################################
+'''
+If we use BPOSD code base, the final shape after HGP will be
+(sqrt(L)**3 * sqrt(L+1)**3, sqrt(L)**3 * sqrt(L)**3 + sqrt(L+1)**3 * sqrt(L+1)**3)
+In stead, we implememt HGP by hand
+
+The target shape is
+(sqrt(L)**3 * sqrt(L)**3, sqrt(L)**3 * sqrt(L)**3 + sqrt(L)**3 * sqrt(L)**3)
+'''
+hx1 = np.kron(h1, np.eye(h2.shape[1]))  # shape(sqrtL**3 * (sqrtL+1)**3, (sqrtL+1)**3 * (sqrtL+1)**3)
+# hx1.reshape(sqrtL*(sqrtL+1), sqrtL*(sqrtL+1), sqrtL*(sqrtL+1), L, L, L)
+print(hx1.shape)
+hx1.reshape(sqrtL, sqrtL+1, sqrtL, sqrtL+1, sqrtL, sqrtL+1, L**3)
+'''Addressing the ghost bits of classical code 2'''
+hx1[:, 0, :, 0:sqrtL, :, 0:sqrtL, :] += hx1[:, sqrtL, :, 0:sqrtL, :, 0:sqrtL, :]  # ghosts in yz-plane
+hx1[:, 0:sqrtL, :, 0, :, 0:sqrtL, :] += hx1[:, 0:sqrtL, :, sqrtL, :, 0:sqrtL, :]  # ghosts in xz-plane
+hx1[:, 0:sqrtL, :, 0:sqrtL, :, 0, :] += hx1[:, 0:sqrtL, :, 0:sqrtL, :, sqrtL, :]  # ghosts in xy-plane
+hx1[:, 0, :, 0, :, 0:sqrtL, :] += hx1[:, sqrtL, :, sqrtL, :, 0:sqrtL, :]  # ghosts on z line
+hx1[:, 0, :, 0:sqrtL, :, 0, :] += hx1[:, sqrtL, :, 0:sqrtL, :, sqrtL, :]  # ghosts on y line
+hx1[:, 0:sqrtL, :, 0, :, 0, :] += hx1[:, 0:sqrtL, :, sqrtL, :, sqrtL, :]  # ghosts on x line
+hx1[:, 0, :, 0, :, 0, :] += hx1[:, sqrtL, :, sqrtL, :, sqrtL, :]  # ghost at corner
+hx1 = hx1[:, 0:sqrtL, :, 0:sqrtL, :, 0:sqrtL, :]  # discard ghosts
+hx1 = hx1.reshape(L**3, L**3)
+print(hx1.shape)
+
+
 # np.savetxt('../data/haah_code_hx_L{}.txt'.format(L), haah_code.hx, fmt='%d')
 # np.savetxt('../data/haah_code_hz_L{}.txt'.format(L), haah_code.hz, fmt='%d')
 
@@ -130,5 +163,31 @@ vars_ghost = np.array([(sqrtL,j,k) for j in range(sqrtL) for k in range(sqrtL)] 
 # fig.savefig('../figures/haah_classical_code2_L{}.png'.format(L))
 # plt.show()
 
-'Visualiing quantum code (X stabilizers)'
-#TODO
+####################################################################################################
+# Debug
+####################################################################################################
+'''Check that the construction is correct
+by checking the ground state degeneracy
+GSD of Haah's code:
+2[1 - 2 q2 + 2^{r+1}(q2 + 12 q15 + 60 q63)],
+for 2<=L<=200, qn(L) = 1 if n divides L, 0 otherwise.
+'''
+# Ls = [4, 8, 16]
+Ls = np.array([8])
+
+def q(n, L):
+    if L > 200:
+        raise NotImplementedError("L > 200 is not implemented.")
+    return 1 if L % n == 0 else 0
+for L in Ls:
+    sqrtL = int(np.sqrt(L))
+    h1, h2 = h1(sqrtL), h2(sqrtL)
+    haah_code = hgp(h1, h2)
+    # print(haah_code.hx.shape)
+    # print(haah_code.k1)
+    # print(haah_code.k1t)
+    # print(haah_code.k2)
+    # print(haah_code.k2t)
+    # gsd = 2*(1 - 2*q(2, L) + 2*L*(q(2, L) + 12*q(15, L) + 60*q(63, L)))
+    log2GSD = 4*L -2 
+    assert haah_code.K == log2GSD, "GSD is not correct."
---
+++ src/gen_xcube.py
@@ -0,0 +1,8 @@
+import numpy as np
+from bposd.hgp import hgp
+from numba import njit
+import matplotlib.pyplot as plt
+
+####################################################################################################
+# Generate checkerboard model. (ref: https://arxiv.org/abs/1101.1962)
+####################################################################################################
