dfc49620b2f157496471d37beada691d3c7f5ac3
Rosetta-Tan <rosetta_tan@outlook.com>
2023-10-05 17:28:11 -0400

    add new stuffs

--- src/energy_barrier_stats.py
+++ src/energy_barrier_stats.py
@@ -1,16 +1,23 @@
 import numpy as np
 from ldpc.code_util import *
+from ldpc.mod2sparse import *
 from bposd.css import *
 from bposd.hgp import hgp
 from numba import njit
 import matplotlib.pyplot as plt
 from timeit import default_timer as timer
+from sys import argv
 
 """
-Greedy algorithm to search for the logical operator
-with the lowest energy barrier.
+Statistical analysis of energy barrier.
 """
 
+# d = int(argv[1])  # toric code
+log2L = int(argv[1])  # Haah's code
+L = 2**log2L
+####################################################################################################
+# Prepare model
+####################################################################################################
 def read_pc(filepath):
     """
     Read parity check matrix from file.
@@ -23,38 +30,70 @@ def read_pc(filepath):
         pc.append(row)
     return np.array(pc, dtype=np.uint8)
 
-hx = read_pc('../data/hx_outputBalancedProduct.txt')
-hz = read_pc('../data/hz_outputBalancedProduct.txt')
-qcode = css_code(hx, hz)  # all kinds of parameters are already obtained during init
-logx_space = row_span(qcode.lx)
-logz_space = row_span(qcode.lz)
+# hx = read_pc(f'../data/toric_code/hx_d{d}.txt')  # toric code
+# hz = read_pc(f'../data/toric_code/hz_d{d}.txt')  # toric code
+hx = read_pc(f'../data/haah_code/hx_L{L}.txt')  # toric code
+hz = read_pc(f'../data/haah_code/hz_L{L}.txt')  # toric code
+qcode = css_code(hx=hx, hz=hz)  # all kinds of parameters are already obtained during init
+# logx_space = row_span(qcode.lx)
+# logz_space = row_span(qcode.lz)
 
-num_sample = 10000
+num_sample = 10
 rng = np.random.default_rng(seed=0)
-################################################################
-
+####################################################################################################
+# Energy cost stats for low weight logical operators
+####################################################################################################
+logz_space = qcode.lz
 min_weight_logz = logz_space[np.argmin(np.sum(logz_space, axis=1))]
-select = np.where(min_weight_logz == 1)[0]
-ps = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+np.save(f'../data/haah_code/min_weight_logz_L{L}.npy', min_weight_logz)
+selector = np.where(min_weight_logz == 1)[0]
+# ps = 1./np.arange(1, len(selector)+1, 1)
+# ps = np.linspace(1/len(selector), 1, num=len(selector)-1,endpoint=True)
+# ps = np.linspace(0.1, 0.9, num=9, endpoint=True)
+print('len of selector: ', len(selector))
+numones = np.arange(1, len(selector), 1)
 synd_weights_list = []
+# synd_weights
 energy_barrier_list = []
-for p in ps:
+for numone in numones:
     qvecs = np.zeros((num_sample, qcode.N), dtype=int)
     synds = np.zeros((num_sample, qcode.N), dtype=int)
     for i in range(num_sample):
-        qvecs[i, select] = rng.choice([0, 1], size=len(select), p=[p, 1-p])
+        posones = rng.choice(range(len(selector)), size=numone, replace=False)
+        bitstring = bin(sum(1<<i for i in posones))[2:].zfill(len(selector))
+        # print(bitstring)
+        qvecs[i, selector] = np.array([int(x) for x in bitstring])
+        # print(qvecs[i])
+        # qvecs[i, selector] = rng.choice([0, 1], size=len(selector), p=[p, 1-p])
     synds = np.mod(qvecs@(hx.T), 2)
     synd_weights = np.sum(synds, axis=1)
     synd_weights_list.append(synd_weights)
     energy_barrier_list.append(np.min(synd_weights))
-################################################################
-# plt.figure()
-# plt.hist(synd_weights, bins=np.linspace(np.min(synd_weights), np.max(synd_weights), 50), density=True)
-# plt.xlabel('Syndrome weight')
-# plt.ylabel('Probability')
-# plt.title('Syndrome weight distribution')
-# plt.savefig('../figures/synd_weight_dist.png', dpi=300)
-# plt.show()
-print(energy_barrier_list)    
+
+print(energy_barrier_list)
+
+####################################################################################################
+# Plot
+####################################################################################################
+fig, ax = plt.subplots()
+ax.scatter(numones, energy_barrier_list,label='energy barrier')
+ax.scatter(numones, np.min(synd_weights_list, axis=1),label='min syndrome weight')
+ax.scatter(numones, np.max(synd_weights_list, axis=1),label='max syndrome weight')
+# plt.hist(synd_weights, bins=np.linspace(np.min(synd_weights), np.max(synd_weights), 50), density=False)
+ax.set_xlabel('Number of ones')
+ax.set_ylabel('Syndrome weight')
+ax.set_title('Syndrome weight distribution')
+fig.savefig(f'../figures/haah_code/synd_weight_dist_L{L}.png', dpi=300)
+plt.show()
 
 
+####################################################################################################
+# Debug
+####################################################################################################
+'''Correct implementation of logical space?'''
+# print('qcode.K: ', qcode.K)
+# print('len of qcode.lx', np.sum(qcode.lz, axis=1))
+# print('number of logical operators: ', len(logx_space))
+# print('logical X operators: ', logx_space)
+# print('logical X space basis: ', qcode.lx)
+# print('test row span: ', row_span(np.array([[1,1,0],[0,1,1]])))
