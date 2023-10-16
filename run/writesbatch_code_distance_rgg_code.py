8c11a79ba7efb7d1d748e96218a670c58fcfdeae
Rosetta-Tan <rosetta_tan@outlook.com>
2023-10-15 21:14:19 -0400

    new stuffs

--- run/writesbatch_code_distance_rgg_code.py
+++ run/writesbatch_code_distance_rgg_code.py
@@ -10,18 +10,19 @@ savedir = '/n/home01/ytan/scratch/qmemory_simulation/data/rgg_code/'
 for s in sizes:
   for r in rs:
     for seed in seeds:
-      file_name = os.path.join(outdir, f'bash_scripts/code_distance_rgg_code_size={s}_r={r}_seed={seed}.sh')
-        with open (file_name, 'w') as rsh:
-          rsh.write('''\
-#!/bin/bash -l
+      file_name = f'sbatch_scripts/code_distance_rgg_code_size={s}_r={r}_seed={seed}.sh'
+      with open (file_name, 'w') as rsh:
+        rsh.write('''\
+#!/bin/bash
 #SBATCH -n 1                # Number of cores
 #SBATCH -N 1                # Ensure that all cores are on one machine
 #SBATCH -t 0-2:00          # Runtime in D-HH:MM, minimum of 10 minutes
-#SBATCH -p cpu_requeue,shared	    # Partition to submit to
+#SBATCH -p serial_requeue,shared	    # Partition to submit to
 #SBATCH --mem=100G          # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o /n/home01/ytan/scratch/qmemory_simulation/output/myoutput_%j.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e /n/home01/ytan/scratch/qmemory_simulation/output/myoutput_%j.err  # File to which STDERR will be written, %j inserts jobid
 
+module load Mambaforge/23.3.1-fasrc01
 mamba activate qec_numerics
 ''')
-          rsh.write(f"python /n/home01/ytan/qmemory_simulation/src/code_distance_rgg_code.py --size {s} --radius {r} --seed {seed} --readdir {readdir} --savedir {savedir}\n")
+        rsh.write(f"python /n/home01/ytan/qmemory_simulation/src/code_distance_rgg_code.py --size {s} --radius {r} --seed {seed} --readdir {readdir} --savedir {savedir}\n")
