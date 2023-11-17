34bae9ea7f408038ea9a15ce2932074ba75c4704
Rosetta-Tan <rosetta_tan@outlook.com>
2023-11-17 15:34:17 -0500

    new

--- src/gen_psi_quad_gen20_brute_force.py
+++ src/gen_psi_quad_gen20_brute_force.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from ldpc.mod2 import row_basis, nullspace, rank
 from helpers_distance import get_classical_code_distance_time_limit, get_classical_code_distance_special_treatment
+from helpers_qc import *
 import json
 import os
 from itertools import product
@@ -22,16 +23,28 @@ num_faces, num_vertices = h.shape
 # post-processing
 ############################################################################################################
 
-columns_to_remove = np.loadtxt('psi_tiling_columns_to_remove_gen20.txt', dtype=int)
-columns_to_remove = np.unique(columns_to_remove)
-columns_to_remove = sorted(columns_to_remove)
+# columns_to_remove = np.loadtxt('psi_tiling_columns_to_remove_gen20_boundary2.txt', dtype=int)
+# columns_to_remove = np.unique(columns_to_remove)
+# columns_to_remove = sorted(columns_to_remove)
+
+# rows_to_remove = np.loadtxt('psi_tiling_rows_to_remove_gen20.txt', dtype=int)
+# rows_to_remove = np.unique(rows_to_remove)
+# rows_to_remove = sorted(rows_to_remove)
+
+boundary_vertices = np.loadtxt('psi_tiling_boundary_vertices_gen20_good_boundary1.txt', dtype=int)
+
+
+
+
+rows_to_remove = []
 
 h = np.delete(h, columns_to_remove, axis=1)
-rows_to_remove = []
+h = np.delete(h, rows_to_remove, axis=0)
+rows_to_remove_additional = []
 for i in range(h.shape[0]):
     if np.sum(h[i, :]) < 3:
-        rows_to_remove.append(i)
-h = np.delete(h, rows_to_remove, axis=0)
+        rows_to_remove_additional.append(i)
+h = np.delete(h, rows_to_remove_additional, axis=0)
 
 # create a mapping between the old and new indices
 old_to_new = {}
@@ -75,8 +88,8 @@ ax.scatter(xs_after_removal, ys_after_removal, marker='o')
 # annotate the points
 # for i in range(len(xs_after_removal)):
 #     plt.annotate(new_to_old[i], (xs_after_removal[i], ys_after_removal[i]), zorder=2)
-for i in range(len(xs)):
-    plt.annotate(i, (xs[i], ys[i]), zorder=2)
+# for i in range(len(xs)):
+#     plt.annotate(i, (xs[i], ys[i]), zorder=2)
 
 for edge in edges:
     plt.plot([xs[edge[0]], xs[edge[1]]], [ys[edge[0]], ys[edge[1]]], color='gray', alpha=0.5, zorder=0)
@@ -88,9 +101,16 @@ for edge in edges:
 
 for edge in edges_after_removal:
         plt.plot([xs[edge[0]], xs[edge[1]]], [ys[edge[0]], ys[edge[1]]], color='black', zorder=0)
+
+face_xs = faces_pos[:, 0]
+face_ys = faces_pos[:, 1]
+plt.scatter(face_xs, face_ys, marker='s', color='red', zorder=0)
+for i in range(len(faces_pos)):
+    plt.annotate(i, (face_xs[i], face_ys[i]), zorder=2)
+
 ax.set_aspect('equal')
 ax.set_axis_off()
-fig.set_size_inches(50,50)
+fig.set_size_inches(50, 50)
 fig.savefig(os.path.join(savedir, 'visualize_patch_good_boundary1.pdf'))
 
 
@@ -98,42 +118,74 @@ fig.savefig(os.path.join(savedir, 'visualize_patch_good_boundary1.pdf'))
 # visualize the logical operators
 ############################################################################################################
 
-for i in range(logical_op_coeffs.shape[0]):
-# for i in range(2):
-    logical_op = (logical_op_coeffs[i]@logical_basis).flatten()
-    # d_bound, logical_op = get_classical_code_distance_special_treatment(h, target_weight=target_weight)
-    # print(f'logical_op {i}: ', logical_op)
-    pos_ones = np.where(logical_op == 1)[0]
-    # print(f'pos_ones {i}: ', pos_ones)
-    pos_ones = [new_to_old[j] for j in pos_ones]
-    print('positions of one in the logical op (original indices): ', pos_ones)
+'''Visualize all logical operators'''
+# for i in range(logical_op_coeffs.shape[0]):
+# # for i in range(2):
+#     logical_op = (logical_op_coeffs[i]@logical_basis).flatten()
+#     # d_bound, logical_op = get_classical_code_distance_special_treatment(h, target_weight=target_weight)
+#     # print(f'logical_op {i}: ', logical_op)
+#     pos_ones = np.where(logical_op == 1)[0]
+#     # print(f'pos_ones {i}: ', pos_ones)
+#     pos_ones = [new_to_old[j] for j in pos_ones]
+#     print('positions of one in the logical op (original indices): ', pos_ones)
 
-    # visualize the points
-    fig, ax = plt.subplots()
-    # xs_after_removal = [x for i, x in enumerate(xs) if i not in columns_to_remove]
-    # ys_after_removal = [y for i, y in enumerate(ys) if i not in columns_to_remove]
-    ax.scatter(xs_after_removal, ys_after_removal, marker='o')
-    # # annotate the points
-    # for i in range(len(xs)):
-    #     plt.annotate(i, (xs[i], ys[i]), zorder=2)
-    ax.scatter(xs[pos_ones], ys[pos_ones], marker='o', color='pink', zorder=1)
+#     # visualize the points
+#     fig, ax = plt.subplots()
+#     # xs_after_removal = [x for i, x in enumerate(xs) if i not in columns_to_remove]
+#     # ys_after_removal = [y for i, y in enumerate(ys) if i not in columns_to_remove]
+#     ax.scatter(xs_after_removal, ys_after_removal, marker='o')
+#     # # annotate the points
+#     # for i in range(len(xs)):
+#     #     plt.annotate(i, (xs[i], ys[i]), zorder=2)
+#     ax.scatter(xs[pos_ones], ys[pos_ones], marker='o', color='pink', zorder=1)
 
-    for edge in edges:
-        plt.plot([xs[edge[0]], xs[edge[1]]], [ys[edge[0]], ys[edge[1]]], color='gray', alpha=0.5, zorder=0)
+#     for edge in edges:
+#         plt.plot([xs[edge[0]], xs[edge[1]]], [ys[edge[0]], ys[edge[1]]], color='gray', alpha=0.5, zorder=0)
 
-    edges_after_removal = []
-    for edge in edges:
-        if edge[0] not in columns_to_remove and edge[1] not in columns_to_remove:
-            edges_after_removal.append([edge[0], edge[1]])
+#     edges_after_removal = []
+#     for edge in edges:
+#         if edge[0] not in columns_to_remove and edge[1] not in columns_to_remove:
+#             edges_after_removal.append([edge[0], edge[1]])
 
-    for edge in edges_after_removal:
-        plt.plot([xs[edge[0]], xs[edge[1]]], [ys[edge[0]], ys[edge[1]]], color='black', zorder=0)
-    ax.set_aspect('equal')
-    ax.set_axis_off()
-    ax.set_title(f'logical operator {i}')
-    fig.set_size_inches(30,30)
-    savename = f'logical_op_{i}.pdf'
-    savepath = os.path.join(savedir, savename)
-    plt.savefig(savepath, bbox_inches='tight', pad_inches=0)
+#     for edge in edges_after_removal:
+#         plt.plot([xs[edge[0]], xs[edge[1]]], [ys[edge[0]], ys[edge[1]]], color='black', zorder=0)
+#     ax.set_aspect('equal')
+#     ax.set_axis_off()
+#     ax.set_title(f'logical operator {i}')
+#     fig.set_size_inches(30,30)
+#     savename = f'logical_op_{i}.pdf'
+#     savepath = os.path.join(savedir, savename)
+#     plt.savefig(savepath, bbox_inches='tight', pad_inches=0)
 
+'''Visualize low-weight logical operators'''
+d_bound, logical_op = get_classical_code_distance_special_treatment(h, target_weight=h.shape[1]//8)
+pos_ones = np.where(logical_op == 1)[0]
+pos_ones = [new_to_old[j] for j in pos_ones]
+print('positions of one in the logical op (original indices): ', pos_ones)
 
+# visualize the points
+fig, ax = plt.subplots()
+# xs_after_removal = [x for i, x in enumerate(xs) if i not in columns_to_remove]
+# ys_after_removal = [y for i, y in enumerate(ys) if i not in columns_to_remove]
+ax.scatter(xs_after_removal, ys_after_removal, marker='o')
+# # annotate the points
+# for i in range(len(xs)):
+#     plt.annotate(i, (xs[i], ys[i]), zorder=2)
+ax.scatter(xs[pos_ones], ys[pos_ones], marker='o', color='pink', zorder=1)
+
+for edge in edges:
+    plt.plot([xs[edge[0]], xs[edge[1]]], [ys[edge[0]], ys[edge[1]]], color='gray', alpha=0.5, zorder=0)
+
+edges_after_removal = []
+for edge in edges:
+    if edge[0] not in columns_to_remove and edge[1] not in columns_to_remove:
+        edges_after_removal.append([edge[0], edge[1]])
+
+for edge in edges_after_removal:
+    plt.plot([xs[edge[0]], xs[edge[1]]], [ys[edge[0]], ys[edge[1]]], color='black', zorder=0)
+ax.set_aspect('equal')
+ax.set_axis_off()
+fig.set_size_inches(30,30)
+savename = f'low_weight_logical_op.pdf'
+savepath = os.path.join(savedir, savename)
+plt.savefig(savepath, bbox_inches='tight', pad_inches=0)
