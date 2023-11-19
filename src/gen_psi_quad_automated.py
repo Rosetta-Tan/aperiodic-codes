2eff38bd35ec59cf96ca0c8dc5a12b40cba40455
Rosetta-Tan <rosetta_tan@outlook.com>
2023-11-19 13:36:48 -0500

    new

--- src/gen_psi_quad_automated.py
+++ src/gen_psi_quad_automated.py
@@ -14,6 +14,59 @@ def tellme(s):
     plt.title(s)
     plt.draw()
 
+def zoom_factory(ax, max_xlim, max_ylim, base_scale = 2.):
+    def zoom_fun(event):
+        # get the current x and y limits
+        cur_xlim = ax.get_xlim()
+        cur_ylim = ax.get_ylim()
+        xdata = event.xdata # get event x location
+        ydata = event.ydata # get event y location
+        if event.button == 'up':
+            # deal with zoom in
+            scale_factor = 1/base_scale
+            x_scale = scale_factor / 2
+        elif event.button == 'down':
+            # deal with zoom out
+            scale_factor = base_scale
+            x_scale = scale_factor * 2
+        else:
+            # deal with something that should never happen
+            scale_factor = 1
+            print(event.button)
+        # set new limits
+        new_width = (cur_xlim[1] - cur_xlim[0]) * x_scale
+        new_height = (cur_ylim[1] - cur_ylim[0]) * scale_factor
+
+        relx = (cur_xlim[1] - xdata) / (cur_xlim[1] - cur_xlim[0])
+        rely = (cur_ylim[1] - ydata) / (cur_ylim[1] - cur_ylim[0])
+
+        if xdata - new_width * (1 - relx) > max_xlim[0]:
+            x_min = xdata - new_width * (1 - relx)
+        else:
+            x_min = max_xlim[0]
+        if xdata + new_width * (relx) < max_xlim[1]:
+            x_max = xdata + new_width * (relx)
+        else:
+            x_max = max_xlim[1]
+        if ydata - new_height * (1 - rely) > max_ylim[0]:
+            y_min = ydata - new_height * (1 - rely)
+        else:
+            y_min = max_ylim[0]
+        if ydata + new_height * (rely) < max_ylim[1]:
+            y_max = ydata + new_height * (rely)
+        else:
+            y_max = max_ylim[1]
+        ax.set_xlim([x_min, x_max])
+        ax.set_ylim([y_min, y_max])
+        ax.figure.canvas.draw()
+
+    fig = ax.get_figure() # get the figure of interest
+    # attach the call back
+    fig.canvas.mpl_connect('scroll_event',zoom_fun)
+
+    #return the function
+    return zoom_fun
+
 def vertex_is_in_boundary(pt, boundary_pts, eps=1e-5):
     '''
     Note: the boundary pts are in the format of complex numbers, and should be ordered in a clockwise manner
@@ -37,14 +90,14 @@ def vertex_is_in_boundary(pt, boundary_pts, eps=1e-5):
     return inside, which_edge
 
 '''Gen15'''
-readdir = '/Users/yitan/Library/CloudStorage/GoogleDrive-yitan@g.harvard.edu/My Drive/from_cannon/qmemory_simulation/data/qc_code/psi_tiling/'
-savedir = '/Users/yitan/Library/CloudStorage/GoogleDrive-yitan@g.harvard.edu/My Drive/from_cannon/qmemory_simulation/data/qc_code/psi_tiling/gen_15/good_boundary'
-'''Gen 20'''
 # readdir = '/Users/yitan/Library/CloudStorage/GoogleDrive-yitan@g.harvard.edu/My Drive/from_cannon/qmemory_simulation/data/qc_code/psi_tiling/'
-# savedir = '/Users/yitan/Library/CloudStorage/GoogleDrive-yitan@g.harvard.edu/My Drive/from_cannon/qmemory_simulation/data/qc_code/psi_tiling/gen_20/good_boundary1'
+# savedir = '/Users/yitan/Library/CloudStorage/GoogleDrive-yitan@g.harvard.edu/My Drive/from_cannon/qmemory_simulation/data/qc_code/psi_tiling/gen_15/good_boundary'
+'''Gen 20'''
+readdir = '/Users/yitan/Library/CloudStorage/GoogleDrive-yitan@g.harvard.edu/My Drive/from_cannon/qmemory_simulation/data/qc_code/psi_tiling/'
+savedir = '/Users/yitan/Library/CloudStorage/GoogleDrive-yitan@g.harvard.edu/My Drive/from_cannon/qmemory_simulation/data/qc_code/psi_tiling/gen_20/good_boundary1'
 if not os.path.exists(savedir):
     os.makedirs(savedir)
-data = np.load(os.path.join(readdir, 'psi_tiling_gen_15.npz'))
+data = np.load(os.path.join(readdir, 'psi_tiling_gen_20.npz'))
 # data = np.load(os.path.join(readdir, 'psi_tiling_gen_20.npz'))
 
 h = data['h']  # equivalent to face_to_vertex
@@ -100,6 +153,10 @@ ax.plot([boundary_vertices[-1][0], boundary_vertices[0][0]], [boundary_vertices[
 ############################################################################################################
 
 tellme('Select points on the boundary with mouse or whitespace. Points must go in clockwise direction. Press enter to finish')
+max_xlim = ax.get_xlim() # get current x_limits to set max zoom out
+max_ylim = ax.get_ylim() # get current y_limits to set max zoom out
+f = zoom_factory(ax, max_xlim, max_ylim, base_scale=1.1)
+# show()
 boundary_vertices = plt.ginput(n=-1, timeout=-1)
 boundary_vertices = np.asarray(boundary_vertices)
 # print('boundary vertices: ', boundary_vertices)
@@ -108,14 +165,15 @@ np.savetxt('psi_tiling_boundary_vertices_gen15.txt', boundary_vertices, fmt='%f'
 for i in range(len(boundary_vertices)-1):
     plt.plot([boundary_vertices[i][0], boundary_vertices[i+1][0]], [boundary_vertices[i][1], boundary_vertices[i+1][1]], color='red', zorder=0)
 plt.plot([boundary_vertices[-1][0], boundary_vertices[0][0]], [boundary_vertices[-1][1], boundary_vertices[0][1]], color='red', zorder=0)
+
 plt.draw()
 
 ############################################################################################################
 # Post processing
 ############################################################################################################
 
-shape = Polygon(boundary_vertices);
-columns_to_remove = [iv for iv, v in enumerate(vertices_pos) if not shape.contains(Point(v[0],v[1]))];
+shape = Polygon(boundary_vertices)
+columns_to_remove = [iv for iv, v in enumerate(vertices_pos) if not shape.contains(Point(v[0],v[1]))]
 rows_to_remove = []
 
 h = np.delete(h, columns_to_remove, axis=1)
@@ -135,14 +193,6 @@ for i in range(num_vertices):
         old_to_new[i] = new_index
         new_to_old[new_index] = i
         new_index += 1
-
-for i in range(num_vertices):
-    if i == 302:
-        plt.scatter(vertices_pos[i][0], vertices_pos[i][1], marker='o', color='green', zorder=1)
-        in_boundary, which_edge = vertex_is_in_boundary(vertices_pos[i], boundary_vertices)
-        print('i = ', i, 'in boundary: ', in_boundary, 'which_edge: ', which_edge)
-        plt.plot([boundary_vertices[which_edge][0], boundary_vertices[which_edge+1][0]], [boundary_vertices[which_edge][1], boundary_vertices[which_edge+1][1]], color='green', zorder=0)
-
 
 xs_after_removal = [x for i, x in enumerate(xs) if i not in columns_to_remove]
 ys_after_removal = [y for i, y in enumerate(ys) if i not in columns_to_remove]
