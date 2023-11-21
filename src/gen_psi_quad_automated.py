aba32e618faee2f8dfd7b4d4665749a92b041338
Rosetta-Tan <rosetta_tan@outlook.com>
2023-11-20 21:08:13 -0500

    for the purpose of sync from windows pc back to macbook

--- src/gen_psi_quad_automated.py
+++ src/gen_psi_quad_automated.py
@@ -92,15 +92,26 @@ def vertex_is_in_boundary(pt, boundary_pts, eps=1e-5):
     return inside, which_edge
 
 '''Gen15'''
-# readdir = '/Users/yitan/Library/CloudStorage/GoogleDrive-yitan@g.harvard.edu/My Drive/from_cannon/qmemory_simulation/data/qc_code/psi_tiling/'
-# savedir = '/Users/yitan/Library/CloudStorage/GoogleDrive-yitan@g.harvard.edu/My Drive/from_cannon/qmemory_simulation/data/qc_code/psi_tiling/gen_15/good_boundary'
+# # readdir = '/Users/yitan/Library/CloudStorage/GoogleDrive-yitan@g.harvard.edu/My Drive/from_cannon/qmemory_simulation/data/qc_code/psi_tiling/'
+# # savedir = '/Users/yitan/Library/CloudStorage/GoogleDrive-yitan@g.harvard.edu/My Drive/from_cannon/qmemory_simulation/data/qc_code/psi_tiling/gen_15/good_boundary'\
+# readdir = '..\data\qc_code\psi_tiling'
+# savedir = '..\\figures\qc_code\psi_tiling\gen_15\\boundary_trial2'
+# data = np.load(os.path.join(readdir, 'psi_tiling_gen_15.npz'))
 '''Gen 20'''
-readdir = '/Users/yitan/Library/CloudStorage/GoogleDrive-yitan@g.harvard.edu/My Drive/from_cannon/qmemory_simulation/data/qc_code/psi_tiling/'
-savedir = '/Users/yitan/Library/CloudStorage/GoogleDrive-yitan@g.harvard.edu/My Drive/from_cannon/qmemory_simulation/data/qc_code/psi_tiling/gen_20/good_boundary1'
+# # readdir = '/Users/yitan/Library/CloudStorage/GoogleDrive-yitan@g.harvard.edu/My Drive/from_cannon/qmemory_simulation/data/qc_code/psi_tiling/'
+# # savedir = '/Users/yitan/Library/CloudStorage/GoogleDrive-yitan@g.harvard.edu/My Drive/from_cannon/qmemory_simulation/data/qc_code/psi_tiling/gen_20/good_boundary1'
+# readdir = '..\data\qc_code\psi_tiling'
+# savedir = '..\\figures\qc_code\psi_tiling\gen_20\\type_5_horizontal'
+# data = np.load(os.path.join(readdir, 'psi_tiling_gen_20.npz'))
+'''Gen 21'''
+# readdir = '/Users/yitan/Library/CloudStorage/GoogleDrive-yitan@g.harvard.edu/My Drive/from_cannon/qmemory_simulation/data/qc_code/psi_tiling/'
+# savedir = '/Users/yitan/Library/CloudStorage/GoogleDrive-yitan@g.harvard.edu/My Drive/from_cannon/qmemory_simulation/data/qc_code/psi_tiling/gen_21/good_boundary'
+readdir = '..\data\qc_code\psi_tiling'
+savedir = '..\\figures\qc_code\psi_tiling\gen_21\\type_5_horizontal'
+data = np.load(os.path.join(readdir, 'psi_tiling_gen_21.npz'))
 if not os.path.exists(savedir):
     os.makedirs(savedir)
-# data = np.load(os.path.join(readdir, 'psi_tiling_gen_15.npz'))
-data = np.load(os.path.join(readdir, 'psi_tiling_gen_20.npz'))
+
 
 h = data['h']  # equivalent to face_to_vertex
 vertices_pos = data['vertices_pos']
@@ -134,24 +145,17 @@ fig.tight_layout()
 # boundary cut by loading from file
 ############################################################################################################
 
-boundary_vertices = np.loadtxt('psi_tiling_boundary_vertices_gen20_good_trial1.txt')
-# boundary_vertices = np.loadtxt('psi_tiling_boundary_vertices_gen15_good_trial1.txt')
-for i in range(len(boundary_vertices)-1):
-    ax.plot([boundary_vertices[i][0], boundary_vertices[i+1][0]], [boundary_vertices[i][1], boundary_vertices[i+1][1]], color='red', zorder=0)
-ax.plot([boundary_vertices[-1][0], boundary_vertices[0][0]], [boundary_vertices[-1][1], boundary_vertices[0][1]], color='red', zorder=0)
-
-# columns_to_remove = np.loadtxt('psi_tiling_columns_to_remove_gen20_boundary2.txt', dtype=int)
-# columns_to_remove = np.unique(columns_to_remove)
-# columns_to_remove = sorted(columns_to_remove)
-
-# rows_to_remove = np.loadtxt('psi_tiling_rows_to_remove_gen20.txt', dtype=int)
-# rows_to_remove = np.unique(rows_to_remove)
-# rows_to_remove = sorted(rows_to_remove)
+# boundary_vertices = np.loadtxt('psi_tiling_boundary_vertices_gen15_type5_horizontal.txt')
+# boundary_vertices = np.loadtxt('psi_tiling_boundary_vertices_gen20_type5_horizontal.txt')
+# boundary_vertices = np.loadtxt('psi_tiling_boundary_vertices_gen21_type5_horizontal.txt')
+# for i in range(len(boundary_vertices)-1):
+#     ax.plot([boundary_vertices[i][0], boundary_vertices[i+1][0]], [boundary_vertices[i][1], boundary_vertices[i+1][1]], color='red', zorder=0)
+# ax.plot([boundary_vertices[-1][0], boundary_vertices[0][0]], [boundary_vertices[-1][1], boundary_vertices[0][1]], color='red', zorder=0)
 
 ############################################################################################################
 # boundary cut by ginput
 ############################################################################################################
-'''
+
 tellme('Select points on the boundary with mouse or whitespace. Points must go in clockwise direction. Press enter to finish')
 max_xlim = ax.get_xlim() # get current x_limits to set max zoom out
 max_ylim = ax.get_ylim() # get current y_limits to set max zoom out
@@ -167,7 +171,7 @@ for i in range(len(boundary_vertices)-1):
 plt.plot([boundary_vertices[-1][0], boundary_vertices[0][0]], [boundary_vertices[-1][1], boundary_vertices[0][1]], color='red', zorder=0)
 
 plt.draw()
-'''
+
 ############################################################################################################
 # Post processing
 ############################################################################################################
@@ -177,12 +181,10 @@ columns_to_remove = [iv for iv, v in enumerate(vertices_pos) if not shape.contai
 rows_to_remove = []
 
 h = np.delete(h, columns_to_remove, axis=1)
-h = np.delete(h, rows_to_remove, axis=0)
-rows_to_remove_additional = []
 for i in range(h.shape[0]):
     if np.sum(h[i, :]) < 3:
-        rows_to_remove_additional.append(i)
-h = np.delete(h, rows_to_remove_additional, axis=0)
+        rows_to_remove.append(i)
+h = np.delete(h, rows_to_remove, axis=0)
 
 # create a mapping between the old and new indices
 old_to_new = {}
@@ -194,8 +196,21 @@ for i in range(num_vertices):
         new_to_old[new_index] = i
         new_index += 1
 
+# create a mapping between the old and new indices (for faces)
+faces_old_to_new = {}
+faces_new_to_old = {}
+new_index = 0
+for i in range(num_faces):
+    if i not in rows_to_remove:
+        faces_old_to_new[i] = new_index
+        faces_new_to_old[new_index] = i
+        new_index += 1
+
 xs_after_removal = [x for i, x in enumerate(xs) if i not in columns_to_remove]
 ys_after_removal = [y for i, y in enumerate(ys) if i not in columns_to_remove]
+
+faces_xs_after_removal = [x for i, x in enumerate(face_xs) if i not in rows_to_remove]
+faces_ys_after_removal = [y for i, y in enumerate(face_ys) if i not in rows_to_remove]
 
 edges_after_removal = []
 for edge in edges:
@@ -252,48 +267,57 @@ k = h.shape[1] - rank(h)
 # logical_basis = row_basis(nullspace(h))
 # print(logical_basis.shape)
 # logical_op_coeffs = np.asarray(list(product([0, 1], repeat=len(logical_basis))))
-# # for i in range(logical_op_coeffs.shape[0]):
-# # # for i in range(2):
-# #     logical_op = (logical_op_coeffs[i]@logical_basis).flatten()
-# #     # d_bound, logical_op = get_classical_code_distance_special_treatment(h, target_weight=target_weight)
-# #     # print(f'logical_op {i}: ', logical_op)
-# #     pos_ones = np.where(logical_op == 1)[0]
-# #     # print(f'pos_ones {i}: ', pos_ones)
-# #     pos_ones = [new_to_old[j] for j in pos_ones]
-# #     print('positions of one in the logical op (original indices): ', pos_ones)
+# for i in range(logical_op_coeffs.shape[0]):
+# # for i in range(60,64):
+#     logical_op = (logical_op_coeffs[i]@logical_basis).flatten() % 2
+#     pos_ones = np.where(logical_op == 1)[0]
+#     pos_ones = [new_to_old[j] for j in pos_ones]
+#     print('positions of one in the logical op (original indices): ', pos_ones)
 
-# #     # visualize the points
-# #     fig, ax = plt.subplots()
-# #     # xs_after_removal = [x for i, x in enumerate(xs) if i not in columns_to_remove]
-# #     # ys_after_removal = [y for i, y in enumerate(ys) if i not in columns_to_remove]
-# #     ax.scatter(xs_after_removal, ys_after_removal, marker='o')
-# #     # # annotate the points
-# #     # for i in range(len(xs)):
-# #     #     plt.annotate(i, (xs[i], ys[i]), zorder=2)
-# #     ax.scatter(xs[pos_ones], ys[pos_ones], marker='o', color='pink', zorder=1)
+#     # visualize the points
+#     fig, ax = plt.subplots()
+#     ax.scatter(xs_after_removal, ys_after_removal, marker='o')
+#     # # annotate the points
+#     for ipt in range(len(xs)):
+#         if ipt not in columns_to_remove:
+#             plt.annotate(old_to_new[ipt], (xs[ipt], ys[ipt]), color='purple', zorder=2)
 
-# #     for edge in edges:
-# #         plt.plot([xs[edge[0]], xs[edge[1]]], [ys[edge[0]], ys[edge[1]]], color='gray', alpha=0.5, zorder=0)
+#     ax.scatter(faces_xs_after_removal, faces_ys_after_removal, marker='s', color='red', zorder=0)
+#     # # annotate the remaining faces
+#     # for iface in range(num_faces):
+#     #     if iface not in rows_to_remove:
+#     #         plt.annotate(faces_old_to_new[iface], (face_xs[iface], face_ys[iface]), zorder=2)
+    
+#     # plot the codeword
+#     ax.scatter(xs[pos_ones], ys[pos_ones], marker='o', color='pink', zorder=1)
 
-# #     edges_after_removal = []
-# #     for edge in edges:
-# #         if edge[0] not in columns_to_remove and edge[1] not in columns_to_remove:
-# #             edges_after_removal.append([edge[0], edge[1]])
+#     for edge in edges:
+#         plt.plot([xs[edge[0]], xs[edge[1]]], [ys[edge[0]], ys[edge[1]]], color='gray', alpha=0.5, zorder=0)
 
-# #     for edge in edges_after_removal:
-# #         plt.plot([xs[edge[0]], xs[edge[1]]], [ys[edge[0]], ys[edge[1]]], color='black', zorder=0)
-# #     ax.set_aspect('equal')
-# #     ax.set_axis_off()
-# #     ax.set_title(f'logical operator {i}')
-# #     fig.set_size_inches(30,30)
-# #     savename = f'logical_op_{i}.pdf'
-# #     savepath = os.path.join(savedir, savename)
-# #     plt.savefig(savepath, bbox_inches='tight', pad_inches=0)
+#     edges_after_removal = []
+#     for edge in edges:
+#         if edge[0] not in columns_to_remove and edge[1] not in columns_to_remove:
+#             edges_after_removal.append([edge[0], edge[1]])
+
+#     for edge in edges_after_removal:
+#         plt.plot([xs[edge[0]], xs[edge[1]]], [ys[edge[0]], ys[edge[1]]], color='black', zorder=0)
+
+#     for ih in range(h.shape[0]):
+#         for jh in range(h.shape[1]):
+#             if h[ih,jh] == 1:
+#                 ax.plot([faces_xs_after_removal[ih], xs_after_removal[jh]], [faces_ys_after_removal[ih], ys_after_removal[jh]], color='gray', linewidth=2, zorder=-1)
+#     ax.set_aspect('equal')
+#     ax.set_axis_off()
+#     ax.set_title(f'logical operator {i}')
+#     fig.set_size_inches(30,30)
+#     savename = f'logical_op_{i}.pdf'
+#     savepath = os.path.join(savedir, savename)
+#     fig.savefig(savepath, bbox_inches='tight', pad_inches=0)
 
 
 # # '''Visualize low-weight logical operators'''
 fig, ax = plt.subplots()
-d_bound, logical_op = get_classical_code_distance_special_treatment(h, target_weight=get_classical_code_distance_time_limit(h, time_limit=5))
+d_bound, logical_op = get_classical_code_distance_special_treatment(h, target_weight=get_classical_code_distance_time_limit(h, time_limit=60))
 pos_ones = np.where(logical_op == 1)[0]
 pos_ones = [new_to_old[j] for j in pos_ones]
 print('positions of one in the logical op (original indices): ', pos_ones)
@@ -307,8 +331,8 @@ ax.plot([boundary_vertices[-1][0], boundary_vertices[0][0]], [boundary_vertices[
 # ys_after_removal = [y for i, y in enumerate(ys) if i not in columns_to_remove]
 ax.scatter(xs_after_removal, ys_after_removal, marker='o')
 # # annotate the points
-# for i in range(len(xs)):
-#     ax.annotate(i, (xs[i], ys[i]), zorder=2)
+# for ipt in range(len(xs)):
+#     ax.annotate(ipt, (xs[ipt], ys[ipt]), zorder=2)
 ax.scatter(xs[pos_ones], ys[pos_ones], marker='o', color='pink', zorder=1)
 
 for edge in edges:
@@ -325,8 +349,8 @@ ax.set_aspect('equal')
 ax.set_axis_off()
 savename = f'low_weight_logical_op.pdf'
 savepath = os.path.join(savedir, savename)
-fig.set_size_inches(50,50)
-fig.savefig(savename, bbox_inches='tight', pad_inches=0)
+fig.set_size_inches(30,30)
+fig.savefig(savepath, bbox_inches='tight', pad_inches=0)
 
 
 plt.show()
