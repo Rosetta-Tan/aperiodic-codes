9c52fee69f2cc863595645c9a81a2914331a68aa
Rosetta-Tan <rosetta_tan@outlook.com>
2024-05-05 09:39:52 -0400

    refactor: update gitignore for js and include lib

--- utils.js
+++ utils.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const chalk = require('chalk');
 
-// 信息打印
+// print message
 const chalkGreen = msg => console.log(chalk.green(msg))
 const chalkRed = msg => console.log(chalk.red(msg))
 
-// 获取路径(根目录下的绝对路径)
+// get path (absolute path under root directory)
 const joinPath = filePath => path.join(__dirname, filePath)
 
 module.exports = {
