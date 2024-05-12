21192b43dcd3b0d1c7cf36014344943040e837a1
Rosetta-Tan <rosetta_tan@outlook.com>
2024-05-11 23:52:36 -0400

    refactor: jsonify data for canvas drawing; separate logic into smaller chunks

--- static/penrose_laplacian_confinement/index.js
+++ static/penrose_laplacian_confinement/index.js
@@ -52,28 +52,85 @@ function getCanvasScale(canvas, vertices) {
 function removeEventListeners() {
     checkCanvas.onclick = null;
     bitCanvas.onclick = null;
+    chooseChecks.onclick = null;
+    chooseBits.onclick = null;
+}
+
+const config = {
+    "current": {
+        "bit": {
+            "canvas": bitCanvas,
+            "faces": initFaces,
+            "edges": initEdges,
+            "vertices": initVtxs,
+            "hlgtVtxs": [],
+            "stateVec": Array(initVtxs.length).fill(0)
+        },
+        "check": {
+            "canvas": checkCanvas,
+            "faces": initFaces,
+            "edges": initEdges,
+            "vertices": initVtxs,
+            "hlgtVtxs": [],
+            "stateVec": Array(initVtxs.length).fill(0)
+        }
+    },
+    "next": {
+        "bit": {
+            "canvas": bitCanvasNxt,
+            "faces": helpers.GeometryUtils.subdivide(initFaces),
+            "edges": helpers.Tiling.uniqEdges(helpers.GeometryUtils.subdivide(initFaces)),
+            "vertices": helpers.Tiling.uniqVtxs(helpers.GeometryUtils.subdivide(initFaces)),
+            "hlgtVtxs": [],
+            "stateVec": Array(initVtxs.length).fill(0)
+        },
+        "check": {
+            "canvas": checkCanvasNxt,
+            "faces": helpers.GeometryUtils.subdivide(initFaces),
+            "edges": helpers.Tiling.uniqEdges(helpers.GeometryUtils.subdivide(initFaces)),
+            "vertices": helpers.Tiling.uniqVtxs(helpers.GeometryUtils.subdivide(initFaces)),
+            "hlgtVtxs": [],
+            "stateVec": Array(initVtxs.length).fill(0)
+        }
+    }
 }
 
 function drawDefault() {
     let stateVec = Array(initVtxs.length).fill(0);
-    draw(initEdges, initVtxs, stateVec, [], bitCanvas);
-    draw(initEdges, initVtxs, stateVec, [], checkCanvas, 'check');
+    draw(config.current.bit);
+    draw(config.current.check);
+    
     let facesNxt = helpers.GeometryUtils.subdivide(initFaces);
-    let [edgesNxt, verticesNxt, parityCheckNxt, bitVectorNxt, checkVectorNxt, hlgtBitIndsNxt, hlgtCheckIndsNxt] = initData(facesNxt);
+    config.next.bit.faces = facesNxt;
+    config.next.check.faces = facesNxt;
+    let verticesNxt = helpers.Tiling.uniqVtxs(facesNxt);
+    config.next.bit.vertices = verticesNxt;
+    config.next.check.vertices = verticesNxt;
+    let edgesNxt = helpers.Tiling.uniqEdges(facesNxt);
+    config.next.bit.edges = edgesNxt;
+    config.next.check.edges = edgesNxt;
     stateVec = Array(verticesNxt.length).fill(0);
-    draw(edgesNxt, verticesNxt, stateVec, [], bitCanvasNxt);
-    draw(edgesNxt, verticesNxt, stateVec, [], checkCanvasNxt, 'check');
+    config.next.bit.stateVec = stateVec;
+    config.next.check.stateVec = stateVec;
+    draw(config.next.bit);
+    draw(config.next.check);
 }
 
-function draw(edges, vertices, stateVec, hlgtInds, canvas, type = 'bit') {
+function draw(obj) {
+    let canvas = obj.canvas;
     let ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
+    let vertices = obj.vertices;
+    let edges = obj.edges;
+    let stateVec = obj.stateVec;
+    let hlgtVtxs = obj.hlgtVtxs;
+
     // Calculate bounds
     let [minX, minY, scaleX, scaleY] = getCanvasScale(canvas, vertices);
     
     // Draw vertices
-    if (type === 'bit') {
+    if (canvas.id.includes('bit')) {
         ctx.fillStyle = 'blue';
         for (let i = 0; i < vertices.length; i++) {
             if (stateVec[i] === 0) {
@@ -83,17 +140,10 @@ function draw(edges, vertices, stateVec, hlgtInds, canvas, type = 'bit') {
                 ctx.globalAlpha = 1;
             }
             ctx.beginPath();
-            var x = (vertices[i].x - minX) * scaleX + PADDING;
-            var y = (vertices[i].y - minY) * scaleY + PADDING;
+            let x = (vertices[i].x - minX) * scaleX + PADDING;
+            let y = (vertices[i].y - minY) * scaleY + PADDING;
             ctx.arc(x, y, 5, 0, 2 * Math.PI);
             ctx.fill();
-            if (hlgtInds.includes(i)) {
-                ctx.lineWidth = 2;
-                ctx.globalAlpha = 1;
-                ctx.beginPath();
-                ctx.arc(x, y, 5+1, 0, 2 * Math.PI);
-                ctx.stroke();
-            }
         }
     } else {
         ctx.fillStyle = 'green';
@@ -110,10 +160,27 @@ function draw(edges, vertices, stateVec, hlgtInds, canvas, type = 'bit') {
             // draw a square centered at (x,y)
             ctx.rect(x-7.5, y-7.5, 15, 15);
             ctx.fill();
-            if (hlgtInds.includes(i)) {
-                ctx.lineWidth = 2;
-                ctx.globalAlpha = 1;
+        }
+    }
+
+    // Draw hightlighted vertices
+    if (obj.hlgtVtxs.length > 0) {
+        ctx.globalAlpha = 1;
+        ctx.lineWidth = 2;
+        if (obj.canvas.id.includes('bit')) {
+            for (let i = 0; i < hlgtVtxs.length; i++) {
                 ctx.beginPath();
+                let x = (hlgtVtxs[i].x - minX) * scaleX + PADDING;
+                let y = (hlgtVtxs[i].y - minY) * scaleY + PADDING;
+                ctx.arc(x, y, 5+1, 0, 2 * Math.PI);
+                ctx.stroke();
+            }
+        }
+        else {
+            for (let i = 0; i < hlgtVtxs.length; i++) {
+                ctx.beginPath();
+                let x = (hlgtVtxs[i].x - minX) * scaleX + PADDING;
+                let y = (hlgtVtxs[i].y - minY) * scaleY + PADDING;
                 ctx.rect(x-7.5-1, y-7.5-1, 15+2, 15+2);
                 ctx.stroke();
             }
@@ -136,7 +203,7 @@ function draw(edges, vertices, stateVec, hlgtInds, canvas, type = 'bit') {
     }
 }
 
-function nearestVtxIdx(vertices, x, y) {
+function nearestVtxCoords(vertices, x, y) {
     let minDist = Number.MAX_VALUE;
     let minIdx = -1;
     for (let i = 0; i < vertices.length; i++) {
@@ -147,18 +214,7 @@ function nearestVtxIdx(vertices, x, y) {
             minIdx = i;
         }
     }
-    return minIdx;
-}
-
-function initData(faces) {
-    let edges = helpers.Tiling.uniqEdges(faces);
-    let vertices = helpers.Tiling.uniqVtxs(faces);
-    let parityCheck = helpers.Tiling.parityCheck(edges, vertices);
-    let bitVector = Array(vertices.length).fill(0);
-    let checkVector = Array(vertices.length).fill(0);
-    let hlgtBitInds = [];
-    let hlgtCheckInds = [];
-    return [edges, vertices, parityCheck, bitVector, checkVector, hlgtBitInds, hlgtCheckInds];
+    return [vertices[minIdx].x, vertices[minIdx].y];
 }
 
 function updatehlgtCheckIndsNxt(faces, hlghtVtxs) {
@@ -187,19 +243,7 @@ function updatehlgtCheckIndsNxt(faces, hlghtVtxs) {
     }
 }
 
-function updatehlgtBitIndsNxt(verticesNxt, hlghtVtx, hlgtBitIndsNxt) {
-    let idx = verticesNxt.indexOf(hlghtVtx);
-    if (!hlgtBitIndsNxt.includes(idx)) {
-        hlgtBitIndsNxt.push(idx);
-    } else {
-        hlgtBitIndsNxt = hlgtBitIndsNxt.filter((v) => v !== idx);
-    }
-    return hlgtBitIndsNxt;
-}
-
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-
+function handleFormSubmit() {
     let gen = parseInt(input.value, 10);
     if (isNaN(gen)) {
         alert('Please enter a valid integer number');
@@ -212,82 +256,166 @@ form.addEventListener('submit', function(event) {
     for (let i = 0; i < gen; i++) {
         faces = helpers.GeometryUtils.subdivide(faces);
     }
-    let facesNxt = helpers.GeometryUtils.subdivide(faces);
-
-    // initialize data for display and calculation for this generation
-    let [edges, vertices, parityCheck, bitVector, checkVector, hlgtBitInds, hlgtCheckInds] = initData(faces);
-    draw(edges, vertices, bitVector, [], bitCanvas);
-    draw(edges, vertices, checkVector, [] ,checkCanvas, 'check');
-    let [edgesNxt, verticesNxt, parityCheckNxt, bitVectorNxt, checkVectorNxt, hlgtBitIndsNxt, hlgtCheckIndsNxt] = initData(facesNxt);
-    draw(edgesNxt, verticesNxt, bitVectorNxt, [], bitCanvasNxt);
-    draw(edgesNxt, verticesNxt, checkVectorNxt, [], checkCanvasNxt, 'check');
-
-    // add event listeners
-    let currentCanvas = null;
-
-    chooseChecks.onclick = (currentCanvas) => {
-        removeEventListeners();
-        currentCanvas = 'checkCanvas';
-        checkCanvas.style.zIndex = 1;
-        bitCanvas.style.zIndex = 0;
-
-        checkCanvas.onclick = (event) => {
-            let [x, y] = [event.offsetX, event.offsetY]
-            let [minX, minY, scaleX, scaleY] = getCanvasScale(checkCanvas, vertices);
-            x = (x - PADDING) / scaleX + minX;
-            y = (y - PADDING) / scaleY + minY;
-            let idx = nearestVtxIdx(vertices, x, y);            
-            if (!hlgtCheckInds.includes(idx)) {
-                hlgtCheckInds.push(idx);
-            } else {
-                hlgtCheckInds = hlgtCheckInds.filter((v) => v !== idx);
-            }
-            draw(edges, vertices, checkVector, hlgtCheckInds, checkCanvas, 'check');
-            let hlgtChecks = [];
-            for (let i = 0; i < hlgtCheckInds.length; i++) {
-                hlgtChecks.push(vertices[hlgtCheckInds[i]]);
-            }
-            hlgtCheckIndsNxt = updatehlgtCheckIndsNxt(verticesNxt, hlgtChecks);
-            draw(edgesNxt, verticesNxt, checkVectorNxt, hlgtCheckIndsNxt, checkCanvasNxt, 'check');
-        }
+    for (let type in config.current) {
+        obj = config.current[type];
+        obj.faces = faces;
+        obj.edges = helpers.Tiling.uniqEdges(faces);
+        obj.vertices = helpers.Tiling.uniqVtxs(faces);
+        obj.hlgtVtxs = [];
+        obj.stateVec = Array(obj.vertices.length).fill(0);
     }
 
-    chooseBits.onclick = (currentCanvas) => {
-        removeEventListeners();
-        currentCanvas = 'bitCanvas';
-        checkCanvas.style.zIndex = 0;
-        bitCanvas.style.zIndex = 1;
-
-        bitCanvas.onclick = (event) => {
-            let [x, y] = [event.offsetX, event.offsetY]
-            let [minX, minY, scaleX, scaleY] = getCanvasScale(bitCanvas, vertices);
-            x = (x - PADDING) / scaleX + minX;
-            y = (y - PADDING) / scaleY + minY;
-            let idx = nearestVtxIdx(vertices, x, y);
-            if (!hlgtBitInds.includes(idx)) {
-                hlgtBitInds.push(idx);
-            } else {
-                hlgtBitInds = hlgtBitInds.filter((v) => v !== idx);
-            }
-            draw(edges, vertices, bitVector, hlgtBitInds, bitCanvas);
-            // hlgtBitIndsNxt = updatehlgtBitIndsNxt(verticesNxt, vertices[idx], hlgtBitIndsNxt);
-            // draw(edgesNxt, verticesNxt, bitVectorNxt, hlgtBitIndsNxt, bitCanvasNxt);
-        }
-    }
-
-    // reset the bit and check vectors
-    reset.onclick = function() {
-        bitVector = Array(vertices.length).fill(0);
-        checkVector = Array(vertices.length).fill(0);
-        draw(edges, vertices, bitVector, [], bitCanvas);
-        draw(edges, vertices, checkVector, [], checkCanvas, 'check');
-        draw(edgesNxt, verticesNxt, bitVectorNxt, [], bitCanvasNxt);
-        draw(edgesNxt, verticesNxt, checkVectorNxt, [], checkCanvasNxt, 'check');
+    faces = helpers.GeometryUtils.subdivide(faces);
+    for (let type in config.next) {
+        obj = config.next[type];
+        obj.faces = faces;
+        obj.edges = helpers.Tiling.uniqEdges(faces);
+        obj.vertices = helpers.Tiling.uniqVtxs(faces);
+        obj.hlgtVtxs = [];
+        obj.stateVec = Array(obj.vertices.length).fill(0);
     }
 }
-);
+
+function handleChooseSubmit() {
+    // removeEventListeners();
+    console.log('type chosen:', document.activeElement.id);
+    let id = document.activeElement.id;
+    let type = id.includes("Check") ? "check" : "bit";
+    let othertype = type === "check" ? "bit" : "check";
+    let obj = config.current[type];
+    let othobj = config.current[othertype];
+    // set the z order of the current canvas to be higher than the other canvas
+    obj.canvas.style.zIndex = 1;
+    console.log(obj.canvas);
+    othobj.canvas.style.zIndex = 0;
+    console.log(othobj.canvas);
+}
+
+function setHlgtChecksFromCurrent(x, y) {
+    obj = config.current.check;
+    objNxt = config.next.check;
+    objNxt.hlgtVtxs = [new helpers.Vtx(x, y)];
+    // look for the vertex in faces
+    let faces = obj.faces;
+    let foundFaceInd = -1;
+    let foundFaceCtg = -1;
+    let foundFaceVtxInd = -1;
+    for (let i = 0; i < faces.length; i++) {
+        let face = faces[i];
+        let faceVtxs = [face.v1, face.v2, face.v3];
+        for (let j = 0; j < faceVtxs.length; j++) {
+            let vtx = faceVtxs[j];
+            if (vtx.x === x && vtx.y === y) {
+                console.log('foundFaceInd:', i);
+                foundFaceInd = i;
+                foundFaceCtg = face.ctg;
+                foundFaceVtxInd = j+1;
+                break;
+            }
+        }
+    }
+    if (foundFaceCtg == 1) {
+        if (foundFaceVtxInd != 2) {
+            alert('face ctg is 1, but the vertex is not the second vertex');
+            return;
+        }
+        let v1 = faces[foundFaceInd].v1;
+        let v2 = faces[foundFaceInd].v2;
+        let P = v1.add((v2.add(v1.scale(-1))).scale(1/helpers.goldenRatio));
+        objNxt.hlgtVtxs.push(P);
+    } else {
+        if (foundFaceVtxInd == 2) {
+            alert('face ctg is 2, but the vertex is not the first or third vertex');
+            return;
+        } else {
+            let v2 = faces[foundFaceInd].v2;
+            let v3 = faces[foundFaceInd].v3;
+            if (foundFaceVtxInd == 1) {
+                let Q = v2.add((v3.add(v2.scale(-1))).scale(1/helpers.goldenRatio));
+                objNxt.hlgtVtxs.push(Q);
+            } else {
+                let R = v2.add((v1.add(v2.scale(-1))).scale(1/helpers.goldenRatio));
+                objNxt.hlgtVtxs.push(R);
+            }
+        }
+    }
+}
+
+function handleCanvasClick(event, obj) {
+    console.log('getCoords');
+    let vertices = obj.vertices;
+    let [x, y] = [event.offsetX, event.offsetY];
+    let [minX, minY, scaleX, scaleY] = getCanvasScale(bitCanvas, vertices);
+    x = (x - PADDING) / scaleX + minX;
+    y = (y - PADDING) / scaleY + minY;
+    [x, y] = nearestVtxCoords(vertices, x, y);
+    // look for the vertex in hlghtVtxs
+    let hlghtVtxs = obj.hlgtVtxs;
+    let foundInd = -1;
+    for (let i = 0; i < hlghtVtxs.length; i++) {
+        if (hlghtVtxs[i].x === x && hlghtVtxs[i].y === y) {
+            foundInd = i;
+            break;
+        }
+    }
+    if (foundInd == -1) {  // if the vertex is not already highlighted
+        hlghtVtxs.push(new helpers.Vtx(x, y));
+    } else {
+        hlghtVtxs.splice(foundInd, 1);
+    }
+    console.log('hlghtVtxs:', hlghtVtxs);
+    obj.hlghtVtxs = hlghtVtxs;
+    draw(obj);
+    
+    if (obj.canvas.id.includes('check')) {
+        setHlgtChecksFromCurrent(x, y);
+        draw(config.next.check);
+    }
+}
+
+function resetCanvas(obj) {
+    obj.hlgtVtxs = [];
+    obj.stateVec = Array(obj.vertices.length).fill(0);
+    draw(obj);
+}
 
 window.onload = () => {
-    removeEventListeners();
+    // removeEventListeners();
     drawDefault();
+}
+
+form.onsubmit = (event) => {
+    event.preventDefault();
+    handleFormSubmit();
+    for (let key in config) {
+        for (let type in config[key]) {
+            draw(config[key][type]);
+        }
+    }
+}
+
+reset.onclick = () => {
+    for (let key in config) {
+        for (let type in config[key]) {
+            resetCanvas(config[key][type]);
+        }
+    }
+}
+
+chooseChecks.onclick = () => {
+    handleChooseSubmit();
+}
+
+chooseBits.onclick = () => {
+    handleChooseSubmit();
+}
+
+checkCanvas.onclick = (event) => {
+    console.log('checkCanvas clicked');
+    handleCanvasClick(event, config.current.check);
+}
+
+bitCanvas.onclick = (event) => {
+    console.log('bitCanvas clicked');
+    handleCanvasClick(event, config.current.bit);
 }
