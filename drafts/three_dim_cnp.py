115725ab431b8606b86f2320b7edb581a4fa4659
yitan <rosetta_tan@outlook.com>
2024-07-15 17:46:05 -0400

    featur (WIP): add projection to the other type of eigenspace

--- drafts/three_dim_cnp.py
+++ drafts/three_dim_cnp.py
@@ -3,7 +3,6 @@ import numpy as np
 from scipy.spatial import ConvexHull, Delaunay
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-import shapely
 
 def gen_lat(n):
     '''
@@ -149,7 +148,7 @@ def voronoi_project(voronoi, proj_neg, visualize=False):
     
     return pts
 
-def cut(lat_pts, voronoi, proj_neg):
+def cut(lat_pts, voronoi, proj, offset_vec=None):
     '''
     Select lattice points in the 6D lattice.
         - condition: the lattice point is inside the ocnvex hull of
@@ -159,8 +158,9 @@ def cut(lat_pts, voronoi, proj_neg):
         - lattice points in 6D
         voronoi: np.array, shape=(6, 2**6)
         - voronoi cell around origin in 6D
-        proj_neg: np.array, shape=(3, 6)
+        proj: np.array, shape=(3, 6)
         - projection isometry matrix into the negative eigenvalue 3D subspace
+        (default: negative eigenvalue)
     Return:
         cut_pts: np.array, shape=(6, n_cut)
         - selected points in 6D
@@ -171,24 +171,35 @@ def cut(lat_pts, voronoi, proj_neg):
     '''
     # convex hull of projected Voronoi cell in 3D
     # scipy requires pts to be row vectors
-    triacontahedron = ConvexHull((proj_neg @ voronoi).T)
+    triacontahedron = ConvexHull((proj @ voronoi).T)
     
     # Function to check if a point is inside the convex hull
-    def _is_point_in_hull(point, hull):
+    def _is_point_in_hull(point, hull, offset_3D=None):
         # We add the homogeneous coordinate for the point
         point = np.append(point, 1)
         # Loop over each facet of the hull
         for equation in hull.equations:
-            if np.dot(equation, point) > 0:
-                return False
+            if offset_vec is None:
+                if np.dot(equation, point) > 0:
+                    return False
+            else:
+                offset_3D = np.append(offset_3D, 0)
+                if np.dot(equation, (point-offset_3D)) > 0:
+                    return False
         return True
     
     # Select lattice points inside the convex hull
     cut_pts = []
     for i in range(lat_pts.shape[1]):
-        pt_proj = proj_neg @ lat_pts[:, i]
-        if _is_point_in_hull(pt_proj, triacontahedron):
-            cut_pts.append(lat_pts[:, i])
+        pt_proj = proj @ lat_pts[:, i]
+        if offset_vec is not None:
+            offset_3D = proj @ offset_vec.reshape(-1, 1)
+            if _is_point_in_hull(pt_proj, triacontahedron, offset_3D):
+                cut_pts.append(lat_pts[:, i])
+        else:
+            if _is_point_in_hull(pt_proj, triacontahedron):
+                cut_pts.append(lat_pts[:, i])
+    print(f'number of points in the cut: {len(cut_pts)}')
 
     '''
     connect neighboring points in cut_pts 
@@ -211,14 +222,15 @@ def cut(lat_pts, voronoi, proj_neg):
 
     return cut_pts, adjacency_matrix
 
-def project(cut_pts, proj_pos):
+def project(cut_pts, proj):
     '''
-    Project the selected points into the postive eigenvalue 3D subspace.
+    Project the selected points into the selected eigenvalue's 3D subspace.
+    (default: positive eigenvalue)
     Return:
         np.array, shape=(3, n_cut)
         - projected points in
     '''
-    return proj_pos @ cut_pts
+    return proj @ cut_pts
 
 def tiling(n, visualize=False):
     lat_pts = gen_lat(n)
@@ -248,6 +260,39 @@ def tiling(n, visualize=False):
 
     return proj_pts, adjacency_matrix
 
+def tiling_neg(n, visualize=False):
+    lat_pts = gen_lat(n)
+    voronoi = gen_voronoi()
+    # projection isometry matrix into the positive eigenvalue 3D subspace
+    proj_pos = gen_proj_pos()
+    # projection isometry matrix into the negative eigenvalue 3D subspace
+    proj_neg = gen_proj_neg()
+    # cut and project
+    # offset_vec = np.array([0.5, 0.5, 0.5, 0.5, 0.5, 0.5])
+    offset_vec = np.zeros(6)
+    cut_pts, adjacency_matrix = cut(lat_pts, voronoi, proj_pos,
+                                     offset_vec=offset_vec)
+    print(f'number of points in the cut: {cut_pts.shape[1]}')
+    proj_pts = project(cut_pts, proj_neg)
+
+    if visualize:
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.set_box_aspect([1,1,1])
+        ax.scatter(proj_pts[0], proj_pts[1], proj_pts[2])
+        # plot the edges
+        for i in range(proj_pts.shape[1]):
+            for j in range(i+1, proj_pts.shape[1]):
+                if adjacency_matrix[i, j]:
+                    ax.plot([proj_pts[0, i], proj_pts[0, j]],
+                            [proj_pts[1, i], proj_pts[1, j]],
+                            [proj_pts[2, i], proj_pts[2, j]],
+                            color='black')
+        plt.show()
+
+    return proj_pts, adjacency_matrix
+
+
 def test():
     n = 6
     lat_pts = gen_lat(n)
@@ -260,24 +305,48 @@ def test():
         # print(f'dot product: {dot_prod}')
         assert np.isclose(dot_prod, 0), f'dot product: {dot_prod}'
     
+    '''
+    Projecting into negative eigenspace
+    '''
     triacontahedron = ConvexHull((proj_neg @ voronoi).T)
     print(len(triacontahedron.simplices))
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    # make sure the three axes have the same scale
-    # ax.set_box_aspect([1,1,1])
-    # ax.plot_trisurf((proj_neg @ voronoi)[0], (proj_neg @ voronoi)[1], (proj_neg @ voronoi)[2], triangles=triacontahedron.simplices, alpha=0.8)
+    fig, ax = plt.subplots(1, 2, subplot_kw={'projection': '3d'})
+    # ax = fig.add_subplot(111, projection='3d')
+    ax[0].set_box_aspect([1,1,1])
+    ax[0].plot_trisurf(
+        (proj_neg @ voronoi)[0],
+        (proj_neg @ voronoi)[1],
+        (proj_neg @ voronoi)[2],
+        triangles=triacontahedron.simplices, alpha=0.8)
+
+    '''
+    Projecting into positive eigenspace
+    '''
+    tria_pos = ConvexHull((proj_pos @ voronoi).T)
+    print(len(tria_pos.simplices))
+    ax[1].set_box_aspect([1,1,1])
+    ax[1].plot_trisurf(
+        (proj_pos @ voronoi)[0],
+        (proj_pos @ voronoi)[1],
+        (proj_pos @ voronoi)[2],
+        triangles=tria_pos.simplices, alpha=0.8)
+
+    '''
+    Results:
+    - The two convex hulls are the same. This is expected since the two
+    projections both span the bases for an icosahedron.
+    '''
     
-    # make tiling
-    proj_pts, adjacency_matrix = tiling(n, visualize=False)
-    ax.scatter(proj_pts[0], proj_pts[1], proj_pts[2])
-    for i in range(proj_pts.shape[1]):
-        for j in range(i+1, proj_pts.shape[1]):
-            if adjacency_matrix[i, j]:
-                ax.plot([proj_pts[0, i], proj_pts[0, j]],
-                        [proj_pts[1, i], proj_pts[1, j]],
-                        [proj_pts[2, i], proj_pts[2, j]],
-                        color='gray', alpha=0.5)
+    # # make tiling
+    # proj_pts, adjacency_matrix = tiling(n, visualize=False)
+    # ax.scatter(proj_pts[0], proj_pts[1], proj_pts[2])
+    # for i in range(proj_pts.shape[1]):
+    #     for j in range(i+1, proj_pts.shape[1]):
+    #         if adjacency_matrix[i, j]:
+    #             ax.plot([proj_pts[0, i], proj_pts[0, j]],
+    #                     [proj_pts[1, i], proj_pts[1, j]],
+    #                     [proj_pts[2, i], proj_pts[2, j]],
+    #                     color='gray', alpha=0.5)
     
     plt.show()
 
