19dff52eff5a944c50be21fe43a998f17cd07228
yitan <rosetta_tan@outlook.com>
2024-07-25 14:52:17 -0400

    check commutation between 3D codes obtained from cut and project HGP of two Sierpinski codes

--- drafts/cut_and_project/cnp_utils.py
+++ drafts/cut_and_project/cnp_utils.py
@@ -34,3 +34,91 @@ def gen_voronoi(dim):
         np.meshgrid(*([[-0.5, 0.5]] * dim), indexing='ij')
     ).reshape(dim, -1)
     return voronoi
+
+def is_point_in_hull(point, hull, offset_vec=None):
+    # Function to check if a point is inside the convex hull
+    if offset_vec:
+        raise NotImplementedError('Offset vector not implemented')
+    # We add the homogeneous coordinate for the point
+    point = np.append(point, 1)
+    # Loop over each facet of the hull
+    for equation in hull.equations:
+        if offset_vec is None:
+            if np.dot(equation, point) > 0:
+                return False
+        else:
+            offset_3D = np.append(offset_3D, 0)
+            if np.dot(equation, (point-offset_3D)) > 0:
+                return False
+    return True
+
+def gen_proj_pos():
+    '''
+    Generate the projection matrix into the positive eigenvalue 3D subspace
+    shape: (3, 6)
+    Notation follows R. M. K. Dietl and J.-H. Eschenburg,
+    see also Yi's notes page 4.
+    '''
+    costheta = 1/np.sqrt(5)
+    sintheta = 2/np.sqrt(5)
+    # Each vector is a row vector. shape: (1, 3)
+    v6 = np.array([0, 0, np.sqrt(2)])
+    v1 = np.array([np.sqrt(2)*sintheta, 0, np.sqrt(2)*costheta])
+    v2 = np.array([
+        np.sqrt(2)*sintheta*np.cos(2*np.pi/5),
+        np.sqrt(2)*sintheta*np.sin(2*np.pi/5),
+        np.sqrt(2)*costheta
+        ])
+    v3 = np.array([
+        np.sqrt(2)*sintheta*np.cos(4*np.pi/5),
+        np.sqrt(2)*sintheta*np.sin(4*np.pi/5),
+        np.sqrt(2)*costheta
+        ])
+    v4 = np.array([
+        np.sqrt(2)*sintheta*np.cos(6*np.pi/5),
+        np.sqrt(2)*sintheta*np.sin(6*np.pi/5),
+        np.sqrt(2)*costheta
+        ])
+    v5 = np.array([
+        np.sqrt(2)*sintheta*np.cos(8*np.pi/5),
+        np.sqrt(2)*sintheta*np.sin(8*np.pi/5),
+        np.sqrt(2)*costheta
+        ])
+    proj_pos = np.vstack([v1, v2, v3, v4, v5, v6]).T  # shape: (3, 6)
+    
+    return proj_pos
+
+def gen_proj_neg():
+    '''
+    Generate the projection matrix into the negative eigenvalue 3D subspace
+    shape: (3, 6)
+    Notation follows R. M. K. Dietl and J.-H. Eschenburg,
+    see also Yi's notes page 4.
+    '''
+    costheta = 1/np.sqrt(5)
+    sintheta = 2/np.sqrt(5)
+    w6 = np.array([0, 0, -np.sqrt(2)])
+    w1 = np.array([np.sqrt(2)*sintheta, 0, np.sqrt(2)*costheta])
+    w2 = np.array([
+        np.sqrt(2)*sintheta*np.cos(4*np.pi/5),
+        np.sqrt(2)*sintheta*np.sin(4*np.pi/5),
+        np.sqrt(2)*costheta
+        ])
+    w3 = np.array([
+        np.sqrt(2)*sintheta*np.cos(8*np.pi/5),
+        np.sqrt(2)*sintheta*np.sin(8*np.pi/5),
+        np.sqrt(2)*costheta
+        ])
+    w4 = np.array([
+        np.sqrt(2)*sintheta*np.cos(2*np.pi/5),
+        np.sqrt(2)*sintheta*np.sin(2*np.pi/5),
+        np.sqrt(2)*costheta
+        ])
+    w5 = np.array([
+        np.sqrt(2)*sintheta*np.cos(6*np.pi/5),
+        np.sqrt(2)*sintheta*np.sin(6*np.pi/5),
+        np.sqrt(2)*costheta
+        ])
+    proj_neg = np.vstack([w1, w2, w3, w4, w5, w6]).T  # shape: (3, 6)
+
+    return proj_neg
---
+++ tests/test_three_dim_commuting.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append('../drafts/cut_and_project')
+import numpy as np
+import unittest
+from three_dim_commuting import *
+
+class TestSixDimConnectivity(unittest.TestCase):
+    def test_hx_vv(self):
+        n = 3
+        hx_vv = np.load('../data/three_dim_commuting/hx_vv.npy')
+        pt1 = np.array([0, 0, 0, 1, 1, 1])
+        pt2 = np.array([1, 0, 0, 1, 1, 1])
+        self.assertTrue(are_connected(pt1, pt2, hx_vv, n))
+
+        pt1 = np.array([0, 0, 0, 2, 2, 2])
+        pt2 = np.array([1, 0, 0, 2, 2, 2])
+        self.assertTrue(are_connected(pt1, pt2, hx_vv, n))
+        
