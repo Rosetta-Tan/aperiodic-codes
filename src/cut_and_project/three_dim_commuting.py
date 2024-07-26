642d3c2a8d8a500d95cf696a77cd55afbb1285b5
yitan <rosetta_tan@outlook.com>
2024-07-26 13:01:05 -0400

    refactor (WIP): decouple cut and genenrate 3D parity check matrix

--- src/cut_and_project/three_dim_commuting.py
+++ src/cut_and_project/three_dim_commuting.py
@@ -131,6 +131,7 @@ def are_connected(pt1, pt2, parity_check_matrix, n):
 
 def get_neighbors(pt, parity_check_matrix, n):
     '''
+    Get neighbors in 6D.
     '''
     ind = six_dim_coord_to_ind(pt, n)
     neighbor_inds = np.where(parity_check_matrix[ind] == 1)[0]
@@ -138,58 +139,13 @@ def get_neighbors(pt, parity_check_matrix, n):
                  for neighbor_ind in neighbor_inds]
     return neighbor_inds, neighbors
 
-def cut(lat_pts, voronoi, original_parity_check_matrix,
-        proj, n, offset_vec=None):
+def gen_new_pc_matrix(cut_pts,
+                                full_to_cut_ind_map,
+                                original_parity_check_matrix, n):
     '''
-    Select lattice points in the 6D lattice.
-    There are two qubits per vertex in the 6D lattice.
-    A stabilizer will involve both VV and CC type qubits.
-    We keep the feature that there are two qubits per vertex in 3D.
-
-    VV type qubits are the first n**6 qubits.
-    CC type qubits are the last n**6 qubits.
-        - condition: the lattice point is inside the ocnvex hull of
-        the Voronoi unit cell projected to the perpendiculr 3D space.
-    Args:
-        lat_pts: np.array, shape=(6, n**6)
-        - lattice points in 6D
-        voronoi: np.array, shape=(6, 2**6)
-        - voronoi cell around origin in 6D
-        proj: np.array, shape=(3, 6)
-        - projection isometry matrix into the corresponding eigval 3D subspace
-        (default: negative eigenvalue)
-    Return:
-        cut_pts: np.array, shape=(6, n_cut)
-        - selected points in 6D
-        - cut_pts[0]: x0 coordinates
-        - cut_pts[1]: x1 coordinates
-        - ...
-        - cut_pts[5]: x5 coordinates
-    '''
-    # FIXME: decouple cut and obtain new parity-check matrix
-    # TODO: implement offset vector
-
-    if offset_vec is not None:
-        raise NotImplementedError('Offset vector is not implemented yet')
-    # convex hull of projected Voronoi cell in 3D
-    # scipy requires pts to be row vectors
-    triacontahedron = ConvexHull((proj @ voronoi).T)
-    
-    # Select lattice points inside the convex hull
-    full_to_cut_ind_map = {}
-    cut_pts = []
-    for i in range(lat_pts.shape[1]):
-        pt_proj = proj @ lat_pts[:, i]
-        if is_point_in_hull(pt_proj, triacontahedron):
-            full_to_cut_ind_map[i] = len(cut_pts)
-            cut_pts.append(lat_pts[:, i])
-    cut_pts = np.asarray(cut_pts).T # shape=(6, n_cut)
-
-    '''
-    connect neighboring points in cut_pts 
-    according to connectivity in the 6D lattice:
-    if two points are neighbors in the 6D lattice,
-    then they are connected by an edge in the 3D tiling
+    Generate the new parity-check matrix after cutting and projecting.
+    new_parity_check_matrix will contain all-zero rows,
+    purge after combining CC and VV type
     '''
     n_cut = cut_pts.shape[1]
     new_parity_check_matrix = np.zeros((n_cut, n_cut), dtype=int)
@@ -208,22 +164,7 @@ def cut(lat_pts, voronoi, original_parity_check_matrix,
             for i_full_neighbor in neighbor_inds:
                 i_cut_neighbor = full_to_cut_ind_map[i_full_neighbor]
                 new_parity_check_matrix[i_cut, i_cut_neighbor] = 1
-    
-    # new_parity_check_matrix will contain all-zero rows,
-    # purge after combining CC and VV type
-    return cut_pts, new_parity_check_matrix
-
-def project(cut_pts, proj):
-    '''
-    Project the selected points into the selected eigenvalue's 3D subspace.
-    (default: positive eigenvalue)
-    Args:
-        proj: np.array, shape=(3, 6)
-    Return:
-        np.array, shape=(3, n_cut)
-        - projected points
-    '''
-    return proj @ cut_pts
+    return new_parity_check_matrix
 
 def check_comm_after_proj(hx_vv, hx_cc, hz_vv, hz_cc):
     '''
@@ -253,25 +194,19 @@ if __name__ == '__main__':
     np.save(f'../../data/three_dim_commuting/hz_vv_n={n}.npy', hz_vv)
     np.save(f'../../data/three_dim_commuting/hz_cc_n={n}.npy', hz_cc)
 
-    cut_pts, new_hx_vv = cut(lat_pts, voronoi, hx_vv, proj_neg, n)
-    _, new_hx_cc = cut(lat_pts, voronoi, hx_cc, proj_neg, n)
-    _, new_hz_vv = cut(lat_pts, voronoi, hz_vv, proj_neg, n)
-    _, new_hz_cc = cut(lat_pts, voronoi, hz_cc, proj_neg, n)
+    cut_pts, full_to_cut_ind_map = cut(lat_pts, voronoi, hx_vv, proj_neg, n)
     proj_pts = project(cut_pts, proj_pos)
+    new_hx_vv = gen_new_pc_matrix(lat_pts, voronoi, hx_vv, proj_neg, n)
+    new_hx_cc = gen_new_pc_matrix(lat_pts, voronoi, hx_cc, proj_neg, n)
+    new_hz_vv = gen_new_pc_matrix(lat_pts, voronoi, hz_vv, proj_neg, n)
+    new_hz_cc = gen_new_pc_matrix(lat_pts, voronoi, hz_cc, proj_neg, n)
     
+    print(f'shape of proj_pts: {proj_pts.shape}')
+    np.save(f'../../data/three_dim_commuting/proj_pts_n={n}.npy', proj_pts)
     np.save(f'../../data/three_dim_commuting/new_hx_vv_n={n}.npy', new_hx_vv)
     np.save(f'../../data/three_dim_commuting/new_hx_cc_n={n}.npy', new_hx_cc)
     np.save(f'../../data/three_dim_commuting/new_hz_vv_n={n}.npy', new_hz_vv)
     np.save(f'../../data/three_dim_commuting/new_hz_cc_n={n}.npy', new_hz_cc)
-    np.save(f'../../data/three_dim_commuting/proj_pts_n={n}.npy', proj_pts)
-    print(f'shape of proj_pts: {proj_pts.shape}')
-
-    # # Check commutation
-    # for i in range(new_hx.shape[0]):
-    #     for j in range(new_hz.shape[0]):
-    #         if new_hx[:, i].dot(new_hz[:, j]) % 2 != 0:
-    #             print(f'Commutation check failed: {i}, {j}')
-    #             break
 
     # Check commutation
     print(check_comm_after_proj(new_hx_vv, new_hx_cc, new_hz_vv, new_hz_cc))
