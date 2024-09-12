80e4bd2a3ab07bac4466c2f072393ce1b8db8e7f
Brenden Roberts <19916637+brendenroberts@users.noreply.github.com>
2024-09-12 12:41:24 -0400

    Move some duplicated code into util file

--- aperiodic_codes/cut_and_project/cnp_utils.py
+++ aperiodic_codes/cut_and_project/cnp_utils.py
@@ -1,6 +1,20 @@
 # 3D cut and project tiling
 import numpy as np
+from subprocess import run
+import scipy.sparse as sp
+from scipy.linalg import expm
 from scipy.optimize import linprog
+
+def gen_rotation(thetas,d):
+    assert len(thetas) == (d*(d-1))//2, "Must provide d*(d-1)/2 angles";
+    T = np.zeros((d,d),dtype=float);
+    a = 0;
+    for i in range(d):
+        for j in range(i+1,d):
+            T[i,j] = thetas[a];
+            T[j,i] = -thetas[a];
+            a += 1;
+    return expm(T);
 
 def gen_lat(low, high, dim):
     '''
@@ -30,6 +44,55 @@ def gen_voronoi(dim):
     voronoi = np.array(np.meshgrid(*([[-0.5, 0.5]]*dim),indexing='ij')).reshape(dim, -1)
     return voronoi.T
 
+def coord3_to_idx(x, y, z, n):
+    return None if abs(x) > n or abs(y) > n or abs(z) > n else (x+n) * (2*n+1)**2 + (y+n) * (2*n+1) + (z+n);
+
+def idx_to_coord3(idx, n):
+    x = idx // (2*n+1)**2 - n;
+    y = (idx % (2*n+1)**2) // (2*n+1) - n;
+    z = idx % (2*n+1) - n;
+    return x, y, z
+
+def gen_code_3d(spec,n):
+    d3 = np.array([[ 0, 0, 0],
+                   [ 1, 0, 0],[-1, 0, 0],[ 0, 1, 0],[ 0,-1, 0],[ 0, 0, 1],[ 0, 0,-1],
+                   [ 1, 1, 0],[ 1, 0, 1],[ 0, 1, 1],[ 1,-1, 0],[ 1, 0,-1],[ 0, 1,-1],[-1, 1, 0],[-1, 0, 1],[ 0,-1, 1],[-1,-1, 0],[-1, 0,-1],[ 0,-1,-1],
+                   [ 1, 1, 1],[ 1, 1,-1],[ 1,-1, 1],[ 1,-1,-1],[-1, 1, 1],[-1, 1,-1],[-1,-1, 1],[-1,-1,-1]]);
+
+    row = [];
+    col = [];
+    for i in range(-n,n+1):
+        for j in range(-n,n+1):
+            for k in range(-n,n+1):
+                idx = coord3_to_idx(i, j, k, n);
+                cur_col = [y for x in np.where(spec)[0] if (y := coord3_to_idx(*(d3[x]+[i,j,k]),n)) is not None];
+                row = row + [idx]*len(cur_col);
+                col = col + cur_col;
+    return sp.coo_matrix((np.ones_like(row,dtype=int),(row,col)) , shape=((2*n+1)**3,(2*n+1)**3)).tocsc();
+
+def gen_hgp(h1, h2):
+    '''
+    Generate the HGP of H1 and H2.
+    Hx = [H_1 x I_n2, I_m1 x H_2^T]
+    Hz = [H_1^T x I_m2, I_n1 x H_2]
+    Returns:
+        Hx and Hz
+    '''
+    hx = sp.hstack((sp.kron(h1,sp.eye(h2.shape[1])),sp.kron(sp.eye(h1.shape[0]),h2.T.tocsc()))).tocsc();
+    hz = sp.hstack((sp.kron(sp.eye(h1.shape[1]),h2),sp.kron(h1.T.tocsc(),sp.eye(h2.shape[0])))).tocsc();
+    return hx, hz
+
+def cut_ext(lat_pts , voronoi , proj_neg , offset, f_base, nTh):
+    orth_pts = lat_pts @ proj_neg;
+    orth_window = proj_neg.T @ (voronoi + np.tile([offset],(voronoi.shape[0],1))).T;
+    np.savez(f'{f_base}_cut.npz',orth_pts=orth_pts,orth_window=orth_window);
+    run(f'cut_multi {f_base} {nTh}',shell=True);
+    cut_inds = np.load(f'{f_base}_ind.npy');
+    run(f'rm {f_base}_cut.npz',shell=True);
+    run(f'rm {f_base}_ind.npy',shell=True);
+
+    return cut_inds , {cut_inds[i]:i for i in range(len(cut_inds))};
+
 def is_point_in_hull(points, x):
     n_points = len(points)
     n_dim = len(x)
@@ -38,76 +101,6 @@ def is_point_in_hull(points, x):
     b = np.r_[x, np.ones(1)]
     lp = linprog(c, A_eq=A, b_eq=b)
     return lp.success
-
-def gen_proj_pos():
-    '''
-    Generate the projection matrix into the positive eigenvalue 3D subspace
-    shape: (3, 6)
-    Notation follows R. M. K. Dietl and J.-H. Eschenburg
-    '''
-    costheta = 1/np.sqrt(5)
-    sintheta = 2/np.sqrt(5)
-    # Each vector is a row vector. shape: (1, 3)
-    v6 = np.array([0, 0, np.sqrt(2)])
-    v1 = np.array([np.sqrt(2)*sintheta, 0, np.sqrt(2)*costheta])
-    v2 = np.array([
-        np.sqrt(2)*sintheta*np.cos(2*np.pi/5),
-        np.sqrt(2)*sintheta*np.sin(2*np.pi/5),
-        np.sqrt(2)*costheta
-        ])
-    v3 = np.array([
-        np.sqrt(2)*sintheta*np.cos(4*np.pi/5),
-        np.sqrt(2)*sintheta*np.sin(4*np.pi/5),
-        np.sqrt(2)*costheta
-        ])
-    v4 = np.array([
-        np.sqrt(2)*sintheta*np.cos(6*np.pi/5),
-        np.sqrt(2)*sintheta*np.sin(6*np.pi/5),
-        np.sqrt(2)*costheta
-        ])
-    v5 = np.array([
-        np.sqrt(2)*sintheta*np.cos(8*np.pi/5),
-        np.sqrt(2)*sintheta*np.sin(8*np.pi/5),
-        np.sqrt(2)*costheta
-        ])
-    proj_pos = np.vstack([v1, v2, v3, v4, v5, v6])  # shape: (3, 6)
-    
-    return proj_pos
-
-def gen_proj_neg():
-    '''
-    Generate the projection matrix into the negative eigenvalue 3D subspace
-    shape: (3, 6)
-    Notation follows R. M. K. Dietl and J.-H. Eschenburg,
-    see also Yi's notes page 4.
-    '''
-    costheta = 1/np.sqrt(5)
-    sintheta = 2/np.sqrt(5)
-    w6 = np.array([0, 0, -np.sqrt(2)])
-    w1 = np.array([np.sqrt(2)*sintheta, 0, np.sqrt(2)*costheta])
-    w2 = np.array([
-        np.sqrt(2)*sintheta*np.cos(4*np.pi/5),
-        np.sqrt(2)*sintheta*np.sin(4*np.pi/5),
-        np.sqrt(2)*costheta
-        ])
-    w3 = np.array([
-        np.sqrt(2)*sintheta*np.cos(8*np.pi/5),
-        np.sqrt(2)*sintheta*np.sin(8*np.pi/5),
-        np.sqrt(2)*costheta
-        ])
-    w4 = np.array([
-        np.sqrt(2)*sintheta*np.cos(2*np.pi/5),
-        np.sqrt(2)*sintheta*np.sin(2*np.pi/5),
-        np.sqrt(2)*costheta
-        ])
-    w5 = np.array([
-        np.sqrt(2)*sintheta*np.cos(6*np.pi/5),
-        np.sqrt(2)*sintheta*np.sin(6*np.pi/5),
-        np.sqrt(2)*costheta
-        ])
-    proj_neg = np.vstack([w1, w2, w3, w4, w5, w6]).T  # shape: (3, 6)
-
-    return proj_neg
 
 def cut(lat_pts, voronoi, proj, offset_vec=None):
     '''
@@ -166,3 +159,12 @@ def project(cut_pts, proj):
         projected points: np.array, shape=(3, n_cut)
     '''
     return cut_pts @ proj
+
+def check_comm_after_proj(hx_vv, hx_cc, hz_vv, hz_cc,cut_bulk = None):
+    '''
+    Check commutation of all pairs of stabilizers.
+    '''
+    assert hx_vv.shape == hx_cc.shape == hz_vv.shape == hz_cc.shape
+    hx = np.hstack((hx_vv, hx_cc))
+    hz = np.hstack((hz_vv, hz_cc))
+    return np.sum((hx @ hz.T) % 2) if cut_bulk == None else np.sum((hx @ hz.T)[np.ix_(cut_bulk,cut_bulk)] % 2);
